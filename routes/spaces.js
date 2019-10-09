04eb36a27e4cca9aff4b3897a1dd2f107b424ecb
Ryan Townsend <rtownsend@aais.com>
2019-10-09 02:11:56 -0500

    effective date part now working
    part 1 of 2 is getting rooms based on effective dating, added some required variable unit tests as well

--- routes/spaces.js
+++ routes/spaces.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var axios = require('axios');
 const config = require('../config');
 const ReadQueryBuilder = require('../utility/queryBuilderGet');
 const QueryTypeEnum = require('../utility/queryTypeEnum');
-const EntityEnum = require('../utility/entityEnum');
 const CredentialedQuery = require('../utility/credentialedQuery');
 
 /**
@@ -41,53 +39,47 @@ router.get('/rooms/availability', (req, res, next) => {
   // 1) get a list of all rooms available at all for the date(s) 
   // 2) find rooms that are unavailable during the actual meeting time
 
-  const filterStartDate = '2019-10-10T00:00:00'; //req.query.start;
-  const filterEndDate = '2019-10-10T00:00:00'; // req.query.end;
+  let filterStartDate = req.query.start;
+  let filterEndDate = req.query.end;
 
-  // todo fail the API call if either start date or end date not specified
+  if (!filterStartDate || !filterEndDate) {
+    res.sendStatus(400);
+  } else {
+    var qb = new ReadQueryBuilder();
+    qb.sort = '%2BBuilding.Name,Name';
+    qb.queryType = QueryTypeEnum.ADVANCED;  
+    qb.limit = 500;
+    // todo RT extend comparison operations in query builder so we can use paramterized field/value pairs instead of this hacky 'advanced' version: 
+    var start = `EffectiveStartDate<="${filterStartDate}"`;
+    var end = `EffectiveEndDate>="${filterEndDate}"`;
+    var doNotSchedule = 'DoNotSchedule == 0';
 
-  var qb = new ReadQueryBuilder();
-  qb.sort = '%2BBuilding.Name,Name';
-  qb.queryType = QueryTypeEnum.ADVANCED;  
-  qb.limit = 500;
-  // todo RT extend comparison operations in query builder so we can use paramterized field/value pairs instead of this hacky 'advanced' version: 
-  var start = `EffectiveStartDate>="${filterStartDate}"`;
-  var end = `EffectiveEndDate>="${filterEndDate}"`;
-  var doNotSchedule = 'DoNotSchedule == 0';
+    qb.advancedFilter = encodeURIComponent(end + '&&' +  start + '&&' + doNotSchedule);
 
-  qb.advancedFilter = encodeURIComponent(start + '&&' +  end + '&&' + doNotSchedule);
+    const roomsUrl = config.defaultApi.url + config.defaultApi.roomSearchEndpoint + qb.toQueryString()
 
-  // todo consider converting to:  
-  // '/~api/search/room?_dc=1570564904737&start=0&limit=500&_s=1&fields=RowNumber%2CId%2CRoomName%2CRoomDescription%2CRoomNumber%2CRoomTypeName%2CBuildingCode%2CBuildingName%2CCampusName%2CCapacity%2CBuildingRoomNumberRoomName%2CEffectiveDateId%2CCanEdit%2CCanDelete&sortOrder=%2BBuildingRoomNumberRoomName&page=1&sort=%5B%7B%22property%22%3A%22BuildingRoomNumberRoomName%22%2C%22direction%22%3A%22ASC%22%7D%5D'
-
-  const roomsUrl = config.defaultApi.url + config.defaultApi.roomSearchEndpoint + qb.toQueryString()
-
-    var cq = new CredentialedQuery();
-    cq.get(roomsUrl, res).then(function (response) {
-      let roomData = response.data.data;
-      let allrooms = []; 
-      for (let i = 0; i < roomData.length; i++) {
-        allrooms[i] = {};
-        allrooms[i].roomId = roomData[i][0];
-        allrooms[i].roomName = roomData[i][1];
-        allrooms[i].roomNumber = roomData[i][2];
-        allrooms[i].roomType = roomData[i][3];
-        allrooms[i].buildingName = roomData[i][4];
-        allrooms[i].buildingCode = roomData[i][5];
-        allrooms[i].maxOccupancy = roomData[i][6];
-        allrooms[i].isActive = roomData[i][7];
-        allrooms[i].index = i;
-        if (i === 1) {
-          console.log(roomData[i]);
+      var cq = new CredentialedQuery();
+      cq.get(roomsUrl, res).then(function (response) {
+        let roomData = response.data.data;
+        let allrooms = []; 
+        for (let i = 0; i < roomData.length; i++) {
+          allrooms[i] = {};
+          allrooms[i].roomId = roomData[i][0];
+          allrooms[i].roomName = roomData[i][1];
+          allrooms[i].roomNumber = roomData[i][2];
+          allrooms[i].roomType = roomData[i][3];
+          allrooms[i].buildingName = roomData[i][4];
+          allrooms[i].buildingCode = roomData[i][5];
+          allrooms[i].maxOccupancy = roomData[i][6];
+          allrooms[i].isActive = roomData[i][7];
+          allrooms[i].index = i;
         }
-      }
-      console.log(response.data.data.length);
-      console.log(response.data.data[0]);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(allrooms);
-    }).catch(function (error) {
-      res.send(error);
-    });
+        res.setHeader('Content-Type', 'application/json');
+        res.send(allrooms);
+      }).catch(function (error) {
+        res.send(error);
+      });
+    }
 });
 
 module.exports = router;
--- test/spaces-spec.js
+++ test/spaces-spec.js
@@ -3,25 +3,49 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let app = require('../app');
-const expect = require('chai').expect
-const config = require('../config');
 
 chai.use(chaiHttp);
 const moment = require('moment');
 
-// var currentMonthStart = moment().startOf('month').format('YYYY-MM-DD');
-// var currentMonthEnd = moment().endOf('month').format('YYYY-MM-DD');
-// var previousMonthStart = moment().subtract(1,'month').startOf('month').format('YYYY-MM-DD');
-// var previousMonthEnd = moment().subtract(1,'month').endOf('month').format('YYYY-MM-DD');
+var currentDate = moment().startOf('day').format();
 
-describe('/GET available rooms', () => {
-  it.only('it should GET all the rooms available in friendly JSON', (done) => {
+describe.only('/GET available rooms', () => {
+  it('confirm failure if unspecified dates', (done) => {
     chai.request(app)
       .get('/spaces/rooms/availability') // todo RT - need to revisit the restful path
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  }).timeout(15000);
+
+  it('confirm failure if empty start date', (done) => {
+    chai.request(app)
+      .get('/spaces/rooms/availability?end=' + currentDate) // todo RT - need to revisit the restful path
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+      
+  }).timeout(15000);
+
+  it('confirm failure if empty end date', (done) => {
+    chai.request(app)
+      .get('/spaces/rooms/availability??start=' + currentDate)
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  }).timeout(15000);
+
+  it('get all available rooms for today', (done) => {
+    chai.request(app)
+    .get('/spaces/rooms/availability?start=' + currentDate + '&end=' + currentDate)
       .end((err, res) => {
         res.should.have.status(200);
         res.should.be.json;
         done();
       });
   }).timeout(15000);
+
 });
