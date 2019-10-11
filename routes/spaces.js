aed396d30d7deb9fd6815f7ed5afc5a24c876670
Ryan Townsend <rtownsend@aais.com>
2019-10-11 04:45:25 -0500

    part 2 of event posting (will want to squash later)

--- routes/spaces.js
+++ routes/spaces.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var async = require("async");
 var uuidv4 = require('uuidv4').default;
 const config = require('../config');
 const ReadQueryBuilder = require('../utility/queryBuilderGet');
@@ -145,160 +146,151 @@ router.get('/rooms/availability', (req, res, next) => {
  *         schema:
  *           $ref: '#/definitions/Room'
  */
-router.post('/rooms/:id/reservation', (req, res, next) => {
+ router.post('/rooms/:id/reservation', async (req, res, next) => {
   var roomId = req.params.id;
   console.log(roomId);
   let startDate = req.query.start;
   let endDate = req.query.end;
   console.log(startDate + ' # ' + endDate);
-  // res.sendStatus(501);
+
+  // INPUTS
+  const userEmail = 'DemoUser@aais.com';
+  const userName = 'Demo User';
+  const eventName = 'Outlook Test Meeting';
+  // const from = new Date('2019-08-17 01:00:00.000');
+  // const to = new Date('2019-08-17 02:00:00.000');
+  // const roomId = '27e57397-1f8d-47e4-85f1-8963899fa0d9';
+
+  // CONFIG
+  const institutionName = 'AS8DEMO1'; // todo RT - do we need this???????
+  // const baseUrl = 'http://qeapp/SG86044Merced';
+  // const username = 'sysadmin'; // I think this is already being config'd in the bridge API
+  // const password = 'apple';
+  const originatingUserName = 'guest'; // we should look into creating an outlook user and config'ing that
+
+  // const startDate = from.setHours(0,0,0,0);
+  // const endDate = to.setHours(0,0,0,0);
+  // const startMinute = from.getMinutes();
+  // const endMinute = to.getMinutes();
+  const description = `This event was created by ${userName} (${userEmail}) and automatically created here by the Ad Astra Outlook Add-in.`;
+  const currentYear = new Date().getFullYear().toString(); // todo RT -- why do we need this
+
+  // Not really doing anything yet
+  const customerName = 'Outlook';
+  const customerContactName = 'Outlook';
+
+  const eventId = uuidv4();
+  const eventMeetingId = uuidv4();
+  const eventRequestId = uuidv4();
+  const eventRequestMeetingId = uuidv4();
+  const eventMeetingResourceId = uuidv4();
+
+  // axios.defaults.withCredentials = true;
+  // axiosCookieJarSupport(axios);
+  // const cookieJar = new tough.CookieJar();
+
+  // await axios.post(`${baseUrl}/logon.ashx`, { username, password }, { jar: cookieJar }).then((response) => {
+  //     console.log(JSON.stringify(response.data));
+  // }).catch((error) => { console.error(error); });
+
+  let roomNumber = '';
+  let roomName = '';
+  let buildingName = '';
+  let buildingCode = '';
+  let campusName = '';
+  let roomSisKey = '';
+
+  var qb = new ReadQueryBuilder();
+  qb.addFields(['Id', 'Name', 'roomNumber', 'RoomType.Name', 'Building.Name', 'Building.BuildingCode']);
+  qb.addFields(['MaxOccupancy', 'IsActive', 'Building.Campus.Name', 'SisKey']);
+  qb.addFilterFields('Id');
+  qb.addFilterValues(roomId);
 
 
-    // INPUTS
-    const userEmail = 'DemoUser@aais.com';
-    const userName = 'Demo User';
-    const eventName = 'Outlook Test Meeting';
-    // const from = new Date('2019-08-17 01:00:00.000');
-    // const to = new Date('2019-08-17 02:00:00.000');
-    // const roomId = '27e57397-1f8d-47e4-85f1-8963899fa0d9';
 
-    // CONFIG
-    // const institutionName = 'SG86044Merced'; todo RT - do we need this???????
-    // const baseUrl = 'http://qeapp/SG86044Merced';
-    // const username = 'sysadmin'; // I think this is already being config'd in the bridge API
-    // const password = 'apple';
-    const originatingUserName = 'guest'; // we should look into creating an outlook user and config'ing that
-
-    // const startDate = from.setHours(0,0,0,0);
-    // const endDate = to.setHours(0,0,0,0);
-    // const startMinute = from.getMinutes();
-    // const endMinute = to.getMinutes();
-    const description = `This event was created by ${userName} (${userEmail}) and automatically created here by the Ad Astra Outlook Add-in.`;
-    // const currentYear = new Date().getFullYear().toString(); todo RT -- why do we need this
-
-    // Not really doing anything yet
-    const customerName = 'Outlook';
-    const customerContactName = 'Outlook';
-
-    const eventId = uuidv4();
-    const eventMeetingId = uuidv4();
-    const eventRequestId = uuidv4();
-    const eventRequestMeetingId = uuidv4();
-    const eventMeetingResourceId = uuidv4();
-
-    // axios.defaults.withCredentials = true;
-    // axiosCookieJarSupport(axios);
-    // const cookieJar = new tough.CookieJar();
+  var cq = new CredentialedQuery();
+  const roomLookupUrl = config.defaultApi.url + config.defaultApi.roomsEndpoint + qb.toQueryString();  
+  await cq.get(roomLookupUrl, res).then(function (response) {
+    console.log('#### ' + response.data.data); // todo RT remove this 
+    let room = response.data.data[0];
+    roomName = room[1];
+    roomNumber = room[2];
+    buildingName = room[4];
+    buildingCode = room[5];
+    campusName = room[8];
+    roomSisKey = room[9];
+    console.log(`roomName = ${roomName}`);
+    console.log(`roomNumber = ${roomNumber}`);
+    console.log(`buildingName = ${buildingName}`);
+    console.log(`buildingCode = ${buildingCode}`);
+    console.log(`campusName = ${campusName}`);
+    console.log(`roomSisKey = ${roomSisKey}`);
+  }).catch((error) => { console.error(error); });
   
-    // await axios.post(`${baseUrl}/logon.ashx`, { username, password }, { jar: cookieJar }).then((response) => {
-    //     console.log(JSON.stringify(response.data));
-    // }).catch((error) => { console.error(error); });
-
-    let roomNumber = '';
-    let roomName = '';
-    let buildingName = '';
-    let buildingCode = '';
-    let campusName = '';
-    let roomSisKey = '';
-
-    var qb = new ReadQueryBuilder();
-    qb.addFields(['Id', 'Name', 'roomNumber', 'RoomType.Name', 'Building.Name', 'Building.BuildingCode']);
-    qb.addFields(['MaxOccupancy', 'IsActive', 'Building.Campus.Name', 'SisKey']);
-    qb.addFilterFields('Id');
-    qb.addFilterValues(roomId);
-
-//    awaitaxios.get(`${baseUrl}/~api/query/room?fields=
-
-    const roomLookupUrl = config.defaultApi.url + config.defaultApi.roomsEndpoint + qb.toQueryString();
-    console.log('#### ' + roomLookupUrl);
-
-    var cq = new CredentialedQuery();
-    cq.get(roomLookupUrl, res).then(function (response) {
-      // let activityData = response.data.data;
-      // let myresults = createresultlist(activityData);
-      console.log('#### ' + response.data.data);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(response.data.data);
-    }).catch(function (error) {
-      res.send(error);
-    });
-
-    /*await axios.get(`${baseUrl}/~api/query/room?fields=Id%2CName%2CroomNumber%2CRoomType%2EName%2CBuilding%2EName%2CBuilding%2EBuildingCode%2CMaxOccupancy%2CIsActive%2CBuilding%2ECampus%2EName%2CSisKey&filter=Id=="${roomId}"`, { jar: cookieJar }).then((response) => {
-        let room = response.data.data[0];
-        roomName = room[1];
-        roomNumber = room[2];
-        buildingName = room[4];
-        buildingCode = room[5];
-        campusName = room[8];
-        roomSisKey = room[9];
-        console.log(`roomName = ${roomName}`);
-        console.log(`roomNumber = ${roomNumber}`);
-        console.log(`buildingName = ${buildingName}`);
-        console.log(`buildingCode = ${buildingCode}`);
-        console.log(`campusName = ${campusName}`);
-        console.log(`roomSisKey = ${roomSisKey}`);
-    }).catch((error) => { console.error(error); });
-
-    let eventRequestFormId = '';
-    await axios.get(`${baseUrl}/~api/query/EventReqForm?fields=Id,Name&filter=IsActive==1`, { jar: cookieJar }).then((response) => {
-        eventRequestFormId = response.data.data[0][0];
-        console.log(`eventRequestFormId = ${eventRequestFormId}`);
-    }).catch((error) => { console.error(error); });
-
-    let roomConfigurationId = '';
-    await axios.get(`${baseUrl}/~api/query/roomconfiguration?fields=Id%2CIsActive&filter=RoomId=="${roomId}"%26%26IsActive==1%26%26IsDefault==1`, { jar: cookieJar }).then((response) => {
-        roomConfigurationId = response.data.data[0][0];
-        console.log(`roomConfigurationId = ${roomConfigurationId}`);
-    }).catch((error) => { console.error(error); });
-
-    let institutionId = '';
-    await axios.get(`${baseUrl}/~api/query/organization?fields=Id,name,isactive,InstanceName`, { jar: cookieJar }).then((response) => {
-        // Only pull active institutions that match the InstanceName
-        response.data.data.map((institution) => {
-            if (institution[3] == institutionName && institution[2]) {
-                institutionId = institution[0];
-            }    
-        });
-        console.log(`institutionId = ${institutionId}`);
-    }).catch((error) => { console.error(error); });
-
-    let currentMaxRequestNumber = 0;
-    await axios.get(`${baseUrl}/~api/query/eventrequest?fields=RequestNumber&sortOrder=-RequestNumber&Limit=1`, { jar: cookieJar }).then((response) => {
-        response.data.data.map((requestNumber) => {
-            let year = requestNumber[0].split('-')[0];
-            let number = parseInt(requestNumber[0].split('-')[1]);
-            if (year == currentYear && number > currentMaxRequestNumber) {
-                currentMaxRequestNumber = number;
-            }
-        });
-        console.log(`currentMaxRequestNumber = ${currentMaxRequestNumber}`);
-    }).catch((error) => { console.error(error); });
-
-    const requestNumber = `${currentYear}-${(currentMaxRequestNumber + 1).toString().padStart(5, '0')}`;
-    console.log(`requestNumber = ${requestNumber}`);
+  let eventRequestFormId = '';
+  await cq.get(`${config.defaultApi.url}/~api/query/EventReqForm?fields=Id,Name&filter=IsActive==1`, res).then((response) => {
+      eventRequestFormId = response.data.data[0][0];
+      console.log(`eventRequestFormId = ${eventRequestFormId}`);
+  }).catch((error) => { console.error(error); });
 
 
-    var originatingUserId = '';
-    await axios.get(`${baseUrl}/~api/query/User?fields=Id,UserName&filter=Username%3D%3D%22${originatingUserName}%22%26%26IsActive%3D%3D1`, { jar: cookieJar }).then((response) => {
-        originatingUserId = response.data.data[0][0];
-        console.log(`originatingUserId = ${originatingUserId}`);
-    }).catch((error) => { console.error(error); });
+  let roomConfigurationId = '';
+  await cq.get(`${config.defaultApi.url}/~api/query/roomconfiguration?fields=Id%2CIsActive&filter=RoomId=="${roomId}"%26%26IsActive==1%26%26IsDefault==1`, res).then((response) => {
+      roomConfigurationId = response.data.data[0][0];
+      console.log(`roomConfigurationId = ${roomConfigurationId}`);
+  }).catch((error) => { console.error(error); });
+
+  // todo RT - need to revisit institution Id?  
+  let institutionId = '';
+  await cq.get(`${config.defaultApi.url}/~api/query/organization?fields=Id,name,isactive,InstanceName`, res).then((response) => {
+      // Only pull active institutions that match the InstanceName
+      response.data.data.map((institution) => {
+          if (institution[3] == institutionName && institution[2]) {
+              institutionId = institution[0];
+          }    
+      });
+      console.log(`institutionId = ${institutionId}`);
+  }).catch((error) => { console.error(error); });
+
+    
+  let currentMaxRequestNumber = 0;
+  await cq.get(`${config.defaultApi.url}/~api/query/eventrequest?fields=RequestNumber&sortOrder=-RequestNumber&Limit=1`, res).then((response) => {
+      response.data.data.map((requestNumber) => {
+          let year = requestNumber[0].split('-')[0];
+          let number = parseInt(requestNumber[0].split('-')[1]);
+          if (year == currentYear && number > currentMaxRequestNumber) {
+              currentMaxRequestNumber = number;
+          }
+      });
+      console.log(`currentMaxRequestNumber = ${currentMaxRequestNumber}`);
+  }).catch((error) => { console.error(error); });
+
+  const requestNumber = `${currentYear}-${(currentMaxRequestNumber + 1).toString().padStart(5, '0')}`;
+  console.log(`requestNumber = ${requestNumber}`);
+
+  var originatingUserId = '';
+  await cq.get(`${config.defaultApi.url}/~api/query/User?fields=Id,UserName&filter=Username%3D%3D%22${originatingUserName}%22%26%26IsActive%3D%3D1`, res).then((response) => {
+      originatingUserId = response.data.data[0][0];
+      console.log(`originatingUserId = ${originatingUserId}`);
+  }).catch((error) => { console.error(error); });
 
 
-    let reservationNumber = '';
-    await axios.get(`${baseUrl}/~api/events/GetReservationNumber`, { jar: cookieJar }).then((response) => {
-        reservationNumber = response.data;
-        console.log(`reservationNumber = ${reservationNumber}`);
-    }).catch((error) => { console.error(error); });
+  let reservationNumber = '';
+  await cq.get(`${config.defaultApi.url}/~api/events/GetReservationNumber`, res).then((response) => {
+      reservationNumber = response.data;
+      console.log(`reservationNumber = ${reservationNumber}`);
+  }).catch((error) => { console.error(error); });
 
 
-    // TODO Unsure what event type to use for this - is 'Unknown' standard?
-    // await axios.get(`${baseUrl}/~api/query/EventType?fields=Id,Name&filter=IsActive%3D%3D1`).data[0][0];
-    let eventTypeId = null; 
-    let eventTypeName = null;
-    console.log(`eventTypeId = ${eventTypeId}`);
-    console.log(`eventTypeName = ${eventTypeName}`);
+  // TODO Unsure what event type to use for this - is 'Unknown' standard?
+  // await axios.get(`${baseUrl}/~api/query/EventType?fields=Id,Name&filter=IsActive%3D%3D1`).data[0][0];
+  // let eventTypeId = null; 
+  // let eventTypeName = null;
+  // console.log(`eventTypeId = ${eventTypeId}`);
+  // console.log(`eventTypeName = ${eventTypeName}`);
 
+    res.sendStatus(501);
+    /*
 
     const postBody = JSON.stringify({
         "EventRequest": {
@@ -510,6 +502,5 @@ router.post('/rooms/:id/reservation', (req, res, next) => {
     }).catch((error) => { console.error(error); });
 */
 });
-
 
 module.exports = router;
