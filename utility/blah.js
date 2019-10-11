093b7847e64f81fc32f50939edd1f25253b6f645
Ryan Townsend <rtownsend@aais.com>
2019-10-11 04:01:34 -0500

    part 1 of event posting (will want to squash later)

--- utility/blah.js
+++ utility/blah.js
@@ -1,180 +1,38 @@
-var express = require('express');
-var router = express.Router();
+var axios = require('axios').default;
 var uuidv4 = require('uuidv4').default;
-const config = require('../config');
-const ReadQueryBuilder = require('../utility/queryBuilderGet');
-const QueryTypeEnum = require('../utility/queryTypeEnum');
-const CredentialedQuery = require('../utility/credentialedQuery');
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const tough = require('tough-cookie');
 
-/**
- * @swagger
- * /spaces/rooms/availability:
- *   get:
- *     tags:
- *       - rooms
- *     description: Returns all rooms and whether they available for the entire time specified
- *     parameters:
- *       - name: start
- *         description: The beginning date and time 
- *         in: query
- *         required: true
- *         type: string
- *         format: date
- *       - name: end
- *         description: The ending date and time
- *         in: query
- *         required: true
- *         type: string
- *         format: date
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: An array of rooms with their availability specified
- *         schema:
- *           $ref: '#/definitions/Room'
- */
-router.get('/rooms/availability', (req, res, next) => {
+/* 
+ISSUES:
+* Contact empty on event
+* Customer empty on event
+* Contact empty on meeting
+* Event Type empty
 
-  // implementation notes: this is a two step process: 
-  // 1) get a list of all rooms available at all for the date(s) 
-  // 2) find rooms that are unavailable during the actual meeting time
-
-  let filterStartDate = req.query.start;
-  let filterEndDate = req.query.end;
-
-  if (!filterStartDate || !filterEndDate) {
-    res.sendStatus(400);
-  } else {
-    var qb = new ReadQueryBuilder();
-    qb.sort = '%2BBuilding.Name,Name';
-    qb.queryType = QueryTypeEnum.ADVANCED;  
-    qb.limit = 500;
-    // todo RT extend comparison operations in query builder so we can use paramterized field/value pairs instead of this hacky 'advanced' version: 
-    var start = `EffectiveStartDate<="${filterStartDate}"`;
-    var end = `EffectiveEndDate>="${filterEndDate}"`;
-    var doNotSchedule = 'DoNotSchedule == 0';
-
-    qb.advancedFilter = encodeURIComponent(end + '&&' +  start + '&&' + doNotSchedule);
-
-    const roomsUrl = config.defaultApi.url + config.defaultApi.roomSearchEndpoint + qb.toQueryString()
-
-      var cq = new CredentialedQuery();
-      cq.get(roomsUrl, res).then(function (response) {
-        let roomData = response.data.data;
-        let rooms = []; 
-        for (let i = 0; i < roomData.length; i++) {
-          rooms[i] = {};
-          rooms[i].roomId = roomData[i][0];
-          rooms[i].roomBuildingAndNumber = roomData[i][1];
-          rooms[i].whyIsRoomIdHereTwice = roomData[i][2];
-          rooms[i].available = true; // assume this until disproven by retrieving activity list
-        }
-
-        // step 2 is to find conflicting activities so we can mark those rooms as not available
-        //var secondQuery = new ReadQueryBuilder();
-        // temporary hack
-        let end2 = encodeURIComponent(filterStartDate); 
-        let start2 = encodeURIComponent(filterEndDate);
-        let secondaryQuery = 'start=0&limit=500&isForWeekView=false' + 
-          '&fields=ActivityId%2CActivityPk%2CActivityName%2CParentActivityId%2CParentActivityName%2CMeetingType%2CDescription%2CStartDate%2CEndDate%2CDayOfWeek%2CStartMinute%2CEndMinute%2CActivityTypeCode%2CResourceId%2CCampusName%2CBuildingCode%2CRoomNumber%2CRoomName%2CLocationName%2CInstitutionId%2CSectionId%2CSectionPk%2CIsExam%2CIsCrosslist%2CIsAllDay%2CIsPrivate%2CEventId%2CEventPk%2CCurrentState%2CNotAllowedUsageMask%2CUsageColor%2CUsageColorIsPrimary%2CEventTypeColor%2CMaxAttendance%2CActualAttendance%2CCapacity' + 
-          '&entityProps=&_s=1' + 
-          `&filter=(((StartDate%3C%22${start2}%22)%26%26(EndDate%3E%22${end2}%22))%26%26((NotAllowedUsageMask%3D%3Dnull)%7C%7C((NotAllowedUsageMask%268)%3D%3D8)))` +
-          '&sortOrder=%2BStartDate%2C%2BStartMinute&page=1&group=%7B%22property%22%3A%22StartDate%22%2C%22direction%22%3A%22ASC%22%7D&sort=%5B%7B%22property%22%3A%22StartDate%22%2C%22direction%22%3A%22ASC%22%7D%2C%7B%22property%22%3A%22StartMinute%22%2C%22direction%22%3A%22ASC%22%7D%5D'
-
-        const url = config.defaultApi.url + config.defaultApi.calendarWeekGridEndpoint + secondaryQuery;
-        cq.get(url, res).then(function (response) {
-          res.setHeader('Content-Type', 'application/json');
-
-          let data = response.data.data;
-          let unavailableRooms = [];
-          for (let i = 0; i < data.length; i++) {
-            let roomId = data[i][13]
-            unavailableRooms[i] = roomId;
-
-            // this is brute force O(n^2), might want to consider a more elegant solution
-            rooms.forEach(function(item, index) {
-              if (item.roomId === roomId) {
-                item.available = false;
-              }
-            })
-          }
-          res.send(rooms);
-
-        })
-        .catch(function(error) {
-          res.send(error);
-        })
-      }).catch(function (error) {
-        res.send(error);
-      });
-    }
-});
-
-/**
- * @swagger
- * /spaces/rooms/{roomId}/reservation:
- *   post:
- *     tags:
- *       - rooms
- *     description: Reserve the given room for the time duration specified
- *     parameters:
- *       - name: roomId
- *         description: Unique identifier for the room 
- *         in: query
- *         required: true
- *         type: string
- *         format: string
- *       - name: start
- *         description: The beginning date and time 
- *         in: query
- *         required: true
- *         type: string
- *         format: date
- *       - name: end
- *         description: The ending date and time
- *         in: query
- *         required: true
- *         type: string
- *         format: date
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: An array of rooms with their availability specified
- *         schema:
- *           $ref: '#/definitions/Room'
- */
-router.post('/rooms/:id/reservation', (req, res, next) => {
-  var roomId = req.params.id;
-  console.log(roomId);
-  let startDate = req.query.start;
-  let endDate = req.query.end;
-  console.log(startDate + ' # ' + endDate);
-  // res.sendStatus(501);
-
-
+*/
+async function main(){
     // INPUTS
-    const userEmail = 'DemoUser@aais.com';
-    const userName = 'Demo User';
-    const eventName = 'Outlook Test Meeting';
-    // const from = new Date('2019-08-17 01:00:00.000');
-    // const to = new Date('2019-08-17 02:00:00.000');
-    // const roomId = '27e57397-1f8d-47e4-85f1-8963899fa0d9';
+    const userEmail = 'krowan@aais.com';
+    const userName = 'Kyle Rowan';
+    const eventName = 'Outlook Test Meeting 7';
+    const from = new Date('2019-08-17 01:00:00.000');
+    const to = new Date('2019-08-17 02:00:00.000');
+    const roomId = '27e57397-1f8d-47e4-85f1-8963899fa0d9';
 
     // CONFIG
-    // const institutionName = 'SG86044Merced'; todo RT - do we need this???????
-    // const baseUrl = 'http://qeapp/SG86044Merced';
-    // const username = 'sysadmin'; // I think this is already being config'd in the bridge API
-    // const password = 'apple';
+    const institutionName = 'SG86044Merced';
+    const baseUrl = 'http://qeapp/SG86044Merced';
+    const username = 'sysadmin'; // I think this is already being config'd in the bridge API
+    const password = 'apple';
     const originatingUserName = 'guest'; // we should look into creating an outlook user and config'ing that
 
-    // const startDate = from.setHours(0,0,0,0);
-    // const endDate = to.setHours(0,0,0,0);
-    // const startMinute = from.getMinutes();
-    // const endMinute = to.getMinutes();
+    const startDate = from.setHours(0,0,0,0);
+    const endDate = to.setHours(0,0,0,0);
+    const startMinute = from.getMinutes();
+    const endMinute = to.getMinutes();
     const description = `This event was created by ${userName} (${userEmail}) and automatically created here by the Ad Astra Outlook Add-in.`;
-    // const currentYear = new Date().getFullYear().toString(); todo RT -- why do we need this
+    const currentYear = new Date().getFullYear().toString();
 
     // Not really doing anything yet
     const customerName = 'Outlook';
@@ -186,13 +44,13 @@ router.post('/rooms/:id/reservation', (req, res, next) => {
     const eventRequestMeetingId = uuidv4();
     const eventMeetingResourceId = uuidv4();
 
-    // axios.defaults.withCredentials = true;
-    // axiosCookieJarSupport(axios);
-    // const cookieJar = new tough.CookieJar();
+    axios.defaults.withCredentials = true;
+    axiosCookieJarSupport(axios);
+    const cookieJar = new tough.CookieJar();
   
-    // await axios.post(`${baseUrl}/logon.ashx`, { username, password }, { jar: cookieJar }).then((response) => {
-    //     console.log(JSON.stringify(response.data));
-    // }).catch((error) => { console.error(error); });
+    await axios.post(`${baseUrl}/logon.ashx`, { username, password }, { jar: cookieJar }).then((response) => {
+        console.log(JSON.stringify(response.data));
+    }).catch((error) => { console.error(error); });
 
     let roomNumber = '';
     let roomName = '';
@@ -200,30 +58,7 @@ router.post('/rooms/:id/reservation', (req, res, next) => {
     let buildingCode = '';
     let campusName = '';
     let roomSisKey = '';
-
-    var qb = new ReadQueryBuilder();
-    qb.addFields(['Id', 'Name', 'roomNumber', 'RoomType.Name', 'Building.Name', 'Building.BuildingCode']);
-    qb.addFields(['MaxOccupancy', 'IsActive', 'Building.Campus.Name', 'SisKey']);
-    qb.addFilterFields('Id');
-    qb.addFilterValues(roomId);
-
-//    awaitaxios.get(`${baseUrl}/~api/query/room?fields=
-
-    const roomLookupUrl = config.defaultApi.url + config.defaultApi.roomsEndpoint + qb.toQueryString();
-    console.log('#### ' + roomLookupUrl);
-
-    var cq = new CredentialedQuery();
-    cq.get(roomLookupUrl, res).then(function (response) {
-      // let activityData = response.data.data;
-      // let myresults = createresultlist(activityData);
-      console.log('#### ' + response.data.data);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(response.data.data);
-    }).catch(function (error) {
-      res.send(error);
-    });
-
-    /*await axios.get(`${baseUrl}/~api/query/room?fields=Id%2CName%2CroomNumber%2CRoomType%2EName%2CBuilding%2EName%2CBuilding%2EBuildingCode%2CMaxOccupancy%2CIsActive%2CBuilding%2ECampus%2EName%2CSisKey&filter=Id=="${roomId}"`, { jar: cookieJar }).then((response) => {
+    await axios.get(`${baseUrl}/~api/query/room?fields=Id%2CName%2CroomNumber%2CRoomType%2EName%2CBuilding%2EName%2CBuilding%2EBuildingCode%2CMaxOccupancy%2CIsActive%2CBuilding%2ECampus%2EName%2CSisKey&filter=Id=="${roomId}"`, { jar: cookieJar }).then((response) => {
         let room = response.data.data[0];
         roomName = room[1];
         roomNumber = room[2];
@@ -496,7 +331,7 @@ router.post('/rooms/:id/reservation', (req, res, next) => {
                         RequestedSetupTeardown = 5,
                         PartitionConflict = 6,
                         HolidayConflict = 7
-                    * /
+                    */
                     "WorkflowIntent": "S",
                     "WorkflowIntentOwnerId": originatingUserId,
                     "WorkflowState": null
@@ -508,8 +343,6 @@ router.post('/rooms/:id/reservation', (req, res, next) => {
     axios.post(`${baseUrl}/~api/Entity`, postBody, { jar: cookieJar }).then((response) => {
         console.log(JSON.stringify(response.data));
     }).catch((error) => { console.error(error); });
-*/
-});
+}
 
-
-module.exports = router;
+main();
